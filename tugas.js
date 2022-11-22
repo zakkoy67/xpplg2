4a0a8d9201614dd020c2ca0c1ddbc92feee84418
zakkoy67 <118705766+zakkoy67@users.noreply.github.com>
2022-11-22 13:44:09 +0700

    2nd commit

---
+++ tugas.js
@@ -0,0 +1,5 @@
+let i =2;
+
+for(let i = 0; i <=10; i++)
+if(i%2==0)
+{console.log(i)}
---
+++ tugas2.js
@@ -0,0 +1,8 @@
+let text = "";
+let i = 0;
+while (i<10){
+    text +="<br>the number in" + i;
+    i++;
+    if(i%2==0)
+    console.log(i)
+}
